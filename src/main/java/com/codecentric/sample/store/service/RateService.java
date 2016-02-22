2c1102bc4e8a2a2ee631e8d15d859df5f9f127d8
Thomas Jaspers <tritom42@googlemail.com>
2016-02-22 19:35:07 +0100

    Added Powermockito to parametrized tests.

--- src/main/java/com/codecentric/sample/store/service/RateService.java
+++ src/main/java/com/codecentric/sample/store/service/RateService.java
@@ -2,6 +2,7 @@ package com.codecentric.sample.store.service;
 
 import com.codecentric.sample.store.model.Item;
 import com.codecentric.sample.store.repository.ItemRepository;
+import com.codecentric.sample.store.service.tools.StaticService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class RateService {
@@ -13,7 +14,7 @@ public class RateService {
     public int calculateRate(String itemId, int muliplicator) {
 
         Item item = itemRepository.findById(itemId);
-        int rate = item.getPriceInCents() * muliplicator;
+        int rate = item.getPriceInCents() * StaticService.getMultiplicator();
         return rate;
     }
 
--- src/test/java/com/codecentric/sample/store/service/RateServiceTest.java
+++ src/test/java/com/codecentric/sample/store/service/RateServiceTest.java
@@ -2,6 +2,7 @@ package com.codecentric.sample.store.service;
 
 import com.codecentric.sample.store.model.Item;
 import com.codecentric.sample.store.repository.ItemRepository;
+import com.codecentric.sample.store.service.tools.StaticService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,15 +10,28 @@ import org.junit.runners.Parameterized;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
-@RunWith(Parameterized.class)
+// See http://www.jayway.com/2014/11/29/using-another-junit-runner-with-powermock/
+// for an example with the SpringJUnit4ClassRunner.class
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(Parameterized.class)
+@PrepareForTest({StaticService.class})
 public class RateServiceTest {
 
     @Mock
@@ -57,6 +71,8 @@ public class RateServiceTest {
         // Given
         //
         when(itemRepositoryeMock.findById(item.getId())).thenReturn(item);
+        mockStatic(StaticService.class);
+        PowerMockito.when(StaticService.getMultiplicator()).thenReturn(multiplicator);
 
         //
         // When
@@ -66,6 +82,10 @@ public class RateServiceTest {
         //
         // Then
         //
+        verify(itemRepositoryeMock, times(1)).findById(item.getId());
+        verifyStatic(times(1));
+        StaticService.getMultiplicator();
+
         assertThat(result, is(expected));
     }
 }
